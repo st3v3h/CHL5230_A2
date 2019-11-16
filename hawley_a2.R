583472509a7a36820b5620430da70175cf80067d
st3v3h <stevehawley85@gmail.com>
2019-11-16 11:33:29 -0500

    cleaned up and commented

--- hawley_a2.R
+++ hawley_a2.R
@@ -6,7 +6,8 @@
 ####################################
 ########## DATA CLEANING ###########
 ####################################
-library(summarytools)
+
+library(summarytools) #for descriptive statistics
 library(epiR)
 
 #load the data
@@ -40,7 +41,6 @@ cland$ca <- factor(cland$ca, levels = c("0.0","1.0","2.0","3.0"))
 #drop missing values
 cland.cl <- na.omit(cland)
 
-
 #review cleaned data
 str(cland.cl)
 dfSummary(cland.cl)
@@ -52,10 +52,14 @@ dfSummary(cland.cl)
 library(glmnet)
 
 set.seed(10)
+
+#empty vector to collect accuracies
 acc.l <- c()
 
+#repeat validation 5 times
 for (i in 1:5){
-
+  
+  #split into training and test sets
   train.I <- sample(nrow(cland.cl),nrow(cland.cl)*2/3)
   train <- cland.cl[train.I,]
   test <- cland.cl[-train.I,]
@@ -66,37 +70,18 @@ for (i in 1:5){
   x.test <- model.matrix(num ~.,test)[,-1]
   y.test <- test$num
   
+  #create the model
   l.mod <- glmnet(x.train,y.train,family="binomial",alpha=1) # LASSO regression
-  cv.l <- cv.glmnet(x.train,y.train,alpha=1, family="binomial", type.measure = "mse") #optimal lambda
-  #coef.min.l <- coef(cv.l, s = "lambda.min") # coefficients with optimal lambda
-  
+  #use cross validation to fine the optimal lambda
+  cv.l <- cv.glmnet(x.train,y.train,alpha=1, family="binomial", type.measure = "mse") 
+
   predictions <- predict(l.mod, newx = x.test, type = "class", s= cv.l$lambda.min)
 
+  #measure accuracy
   t <- table(predictions, y.test)[2:1,2:1]
   epi.tests(t)
   acc.l[i] <- sum(diag(t))/sum(t) 
   }
-
-# acc.l <- mean(acc.l)
-# acc.l
-
-#build model using k-folds cross validation (k=10 by default). Using LASSO regularization
-# cv.l <- cv.glmnet(x,y,alpha=1,family = "binomial", type.measure = "mse")
-# plot(cv.l,xvar="lambda",label = T)
-# cv.l$lambda.min
-# coef.min <- coef(cv.l, s = "lambda.min")
-# coef.min
-# 
-# predictions.l <- predict(cv.l, newx = x, type = "class", s= cv.l$lambda.min)
-# predictions.l.roc <- predict(cv.l, newx = x, type = "response", s= cv.l$lambda.min)
-# myroc.l <- roc(y ~ predictions.l.roc, data=cland.cl)
-# plot(myroc.l)
-
-# t <- table(predictions.l, y)[2:1,2:1]
-# epi.tests(t)
-# acc <- sum(diag(t))/sum(t) 
-# acc
-
 
 ####################################
 ############### KNN ################
@@ -104,85 +89,60 @@ for (i in 1:5){
 
 library(class)
 
+#create model matrix and scale the predictors
 x <- model.matrix(num ~.,cland.cl)[,-1]
 y <- cland.cl$num
-
 x.sc <- scale(x)
  
-# for (j in 1:50){ 
-#   predictions <- knn.cv(x.sc,y,k=j) 
-#   t <- table(predictions, y)[2:1,2:1] 
-#   acc[j] <- sum(diag(t))/sum(t) 
-#   }
-# k.opt <- max(which(acc==max(acc))) 
-
 set.seed(10)
-acc.j <- c()
+
+#create empty vector for KNN accuracies
 acc.k <- c()
 
+#repeat validation 5 times
 for(i in 1:5){
-train.I <- sample(nrow(cland.cl),nrow(cland.cl)*2/3)
 
-train.data <- x.sc[train.I,]
-test.data <- x.sc[-train.I,]
-labels.train <- y[train.I]
-labels.test <- y[-train.I]
+  #set up training and test sets
+  train.I <- sample(nrow(cland.cl),nrow(cland.cl)*2/3)
+  train.data <- x.sc[train.I,]
+  test.data <- x.sc[-train.I,]
+  labels.train <- y[train.I]
+  labels.test <- y[-train.I]
 
-for (j in 1:50){ 
-  predictions <- knn.cv(train.data,y[train.I],k=j) 
-  t <- table(predictions, y[train.I])[2:1,2:1] 
-  acc.j[j] <- sum(diag(t))/sum(t) 
+  #create empty vector to hold k values from cross validation
+  acc.j <- c()
+
+  #use cross validation to determine optimal k
+  for (j in 1:50){ 
+    predictions <- knn.cv(train.data,y[train.I],k=j) 
+    t <- table(predictions, y[train.I])[2:1,2:1] 
+    acc.j[j] <- sum(diag(t))/sum(t) 
+  }
+
+  #select k with highest accuracy 
+  k.opt <- max(which(acc.j==max(acc.j))) 
+
+  #create model with optimal k
+  predictions <- knn(train.data,test.data,labels.train,k=k.opt)
+  t <- table(predictions, labels.test)[2:1,2:1]
+  epi.tests(t)
+  acc.k[i] <- sum(diag(t))/sum(t) 
 }
-
-k.opt <- max(which(acc.j==max(acc.j))) 
-
-predictions <- knn(train.data,test.data,labels.train,k=k.opt)
-t <- table(predictions, labels.test)[2:1,2:1]
-t
-epi.tests(t)
-acc.k[i] <- sum(diag(t))/sum(t) 
-}
-
-#acc.k <- mean(acc.k)
-#acc.k
-
-# KNN using LOO for cross-validation
-# predictions.knn <- knn.cv(x.sc,y,k=10) 
-# t <- table(predictions.knn, y)[2:1,2:1]
-# epi.tests(t)
-# acc <- sum(diag(t))/sum(t) 
-# acc
 
 ####################################
 ####### CLASSIFICATION TREE ########
 ####################################
 
 library(tree)
-library(pROC)
 
-# cfit2 <- tree(num ~., data = cland.cl) 
-# summary(cfit2)
-# 
-# cv.res <- cv.tree(cfit2, FUN=prune.tree, method = "misclass", K = 5)
-# cv.res
-# 
-# pruned <- prune.misclass(cfit2,best=5) 
-# plot(pruned)
-# text(pruned,pretty=0)
-# 
-# predictions.cvtree <- predict(pruned,newdata=cland.cl,type="class") 
-# t <- table(predictions.cvtree, cland.cl$num)[2:1,2:1] #need to reformat the table for epi.tests
-# epi.tests(t)
-# acc <- sum(diag(t))/sum(t) #ex. (38 + 82) / 146
-# acc
-
-
-
-#Train the tree model
 set.seed(10)
+
+#create empty vector to hold tree accuracies
 acc.t <- c()
 
+#repeat validation 5 times
 for (i in 1:5){
+  #train the tree model with training data
   train.I <- sample(nrow(cland.cl),nrow(cland.cl)*2/3)
   tmp.tree <- tree(num ~. , data = cland.cl,subset=train.I)
   #plot(tmp.tree)
@@ -194,22 +154,25 @@ for (i in 1:5){
   #plot(pruned.train)
   #text(pruned.train,pretty=0)
   
-  #test the model with test data
+  #test the pruned model with test data
   preds3 <- predict(pruned.train,newdata=cland.cl[-train.I,],type="class")
   t <- table(preds3, y[-train.I])[2:1,2:1]
   epi.tests(t)
   acc.t[i] <- sum(diag(t))/sum(t) 
 }
 
-#acc.t <- mean(acc.t)
+####################################
+####### MODEL COMPARISON ###########
+####################################
 
+#gather accuracies from all models into dataframe and compare
 all.acc <- data.frame(LASSO=acc.l, KNN=acc.k, TREE=acc.t)
 boxplot(all.acc, ylab="accuracy")
 summary(all.acc)
 
-
+#perform ANOVA to check for significant differences
 library(reshape2)
 m.acc <- melt(all.acc)
 a1 <- aov(value~variable, data=m.acc)
-summary(a1)
-TukeyHSD(a1)
+summary(a1) #no significant differences
+
